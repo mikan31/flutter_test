3f20d40a387833c5234b7de780181f4947786e11
echigo.kouta <kou11.2612.mikan@gmail.com>
2020-08-23 03:40:01 +0900

    firebase連携の処理の追加

--- sample/lib/routes/chat.dart
+++ sample/lib/routes/chat.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("チャット"),
-      ),
-      body: Center(child: Text("チャット")),
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: AppBar(
+            title: Text("チャット"),
+          ),
+          body: new TestList()),
+    );
+  }
+}
+
+class TestList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('test');
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: users.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return new ListView(
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return new ListTile(
+              title: new Text(document.data()['title']),
+              subtitle: new Text(document.data()['content']),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
