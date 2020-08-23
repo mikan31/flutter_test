0ba785934e28ab41a6b4a0cee7ea948b4d942bd2
pr0ton29 <kamereon.3939@gmail.com>
2020-08-23 16:36:40 +0900

    updated

--- sample/lib/routes/home.dart
+++ sample/lib/routes/home.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:sample/routes/new_transaction_page.dart';
+
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ホーム"),
-      ),
-      body: Center(child: Text("ホーム")),
+        appBar: AppBar(
+          title: Text("ホーム"),
+        ),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewTransaction(),
+                ),
+              );
+            },
+            child: Icon(Icons.add),
+        ),
     );
   }
 }
